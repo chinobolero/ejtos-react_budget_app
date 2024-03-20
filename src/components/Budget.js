2f2dd5cc60f3060ea43b40623df0127607e634b7
David Lopez <divadotrebla@gmail.com>
2024-03-19 22:21:31 -0400

    editable budget

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -5,14 +5,31 @@ import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const upperLimit = 20000;
+
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+
+
+
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        if (event.target.value > upperLimit) {
+            alert("The budget cannot exceed upper limit  (£" + upperLimit + ")  ");
+        } else if (event.target.value < totalExpenses) {
+            alert("You cannot reduce the budget value lower than the spending (£" + totalExpenses + ")  ");
+        }
+        else {
+            setNewBudget(event.target.value);
+        }
+
     }
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        </div>
     );
 };
 export default Budget;
